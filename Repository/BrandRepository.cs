1d1eb94f3180627bd9b527564a2bd29a3e9676e5
Josue Ortiz <ortiz.ochoa.david.josue@gmail.com>
2026-06-18 21:42:10 -0500

    Obtener un elemento por medio de Entity Framework completo

--- Repository/BrandRepository.cs
+++ Repository/BrandRepository.cs
@@ -4,6 +4,7 @@ using Data;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using Microsoft.EntityFrameworkCore;
 
 namespace Repository
 {
@@ -14,5 +15,33 @@ namespace Repository
         {
             _context = context;
         }
+        public async Task<BrandEntity> GetByIdAsync(int id)
+        {
+            var brand = await _context.Brands.FirstOrDefaultAsync(b => b.Id == id);
+            if (brand == null)
+            {
+                throw new KeyNotFoundException($"La marca no existe");
+            }
+
+            return MapToEntity(brand);
+        }
+
+        #region Mappers
+        private static BrandEntity MapToEntity(Brand model)
+        {
+            return new BrandEntity(model.Id, model.Name);
+        }
+
+        private static Brand MapModel(BrandEntity entity)
+        {
+            return new Brand
+            {
+                Id = entity.Id ?? 0,
+                Name = entity.Name
+
+            };
+        }
+        #endregion
+
     }
 }
